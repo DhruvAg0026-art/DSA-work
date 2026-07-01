c3146e4019214a519f3c442b6daf56e9f6c5533b
DhruvAg0026-art <dhruvagrawal0026@gmail.com>
2026-07-01 14:48:59 +0530

    Time: 59 ms (100%), Space: 123.5 MB (31.11%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,78 +1,80 @@
 class Solution {
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size();
+        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1 || n == 1)
+            return 0;
+            
+        int arr[][] = new int[n][n];
 
-        int[][] dist = new int[n][n];
-        for (int[] row : dist)
-            Arrays.fill(row, -1);
-
-        Queue<int[]> q = new LinkedList<>();
-
-        // Multi Source BFS
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid.get(i).get(j) == 1) {
-                    q.offer(new int[]{i, j});
-                    dist[i][j] = 0;
-                }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = grid.get(i).get(j);
             }
         }
-
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, 1, -1};
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            for (int k = 0; k < 4; k++) {
-                int nx = cur[0] + dx[k];
-                int ny = cur[1] + dy[k];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n
-                        && dist[nx][ny] == -1) {
-
-                    dist[nx][ny] = dist[cur[0]][cur[1]] + 1;
-                    q.offer(new int[]{nx, ny});
-                }
+        arr = updateMatrix(arr);
+        
+        int low = 0, high = (n - 1) * 2;
+        int mid = 0; 
+        int result = mid;
+        while(low <= high) {
+            mid = (high - low) / 2 + low;
+            if(dfs(arr,0, 0, mid, n, new boolean[n][n])) {
+                result = mid;
+                low = mid + 1;
             }
+            else high = mid - 1;
         }
-
-        // Max Heap Dijkstra
-        PriorityQueue<int[]> pq =
-                new PriorityQueue<>((a, b) -> b[0] - a[0]);
-
-        boolean[][] vis = new boolean[n][n];
-
-        pq.offer(new int[]{dist[0][0], 0, 0});
-
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-
-            int safe = cur[0];
-            int x = cur[1];
-            int y = cur[2];
-
-            if (x == n - 1 && y == n - 1)
-                return safe;
-
-            if (vis[x][y])
-                continue;
-
-            vis[x][y] = true;
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-
-                if (nx >= 0 && ny >= 0 && nx < n && ny < n
-                        && !vis[nx][ny]) {
-
-                    int newSafe = Math.min(safe, dist[nx][ny]);
-                    pq.offer(new int[]{newSafe, nx, ny});
-                }
-            }
-        }
-
-        return 0;
+        return result;
     }
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                matrix[row][col] ^= 1;
+                if (matrix[row][col] == 1) {
+                    matrix[row][col] = Integer.MAX_VALUE / 2;
+                }
+            }
+        }
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 1; col < n; col++) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col - 1] + 1);
+            }
+            for (int col = n - 2; col >= 0; col--) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row][col + 1] + 1);
+            }
+        }
+
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < n; row++) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row - 1][col] + 1);
+            }
+            for (int row = n - 2; row >= 0; row--) {
+                matrix[row][col] = Math.min(matrix[row][col], matrix[row + 1][col] + 1);
+            }
+        }
+
+        return matrix;
+    }
+    
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    boolean dfs(int arr[][], int r, int c, int mid, int n, boolean[][] visited) {
+        if(visited[r][c] || arr[r][c] < mid) return false;
+        if(r == n - 1 && c == n - 1) return true;
+
+        visited[r][c] = true;
+        for(int i = 0; i < 4; i++) {
+            int x = r + dx[i];
+            int y = c + dy[i];
+
+            if(x == -1 || y == -1 || x == n || y == n) continue;
+            if(dfs(arr, x, y, mid, n, visited)) return true;
+        }
+        return false;
+    }
+
 }
